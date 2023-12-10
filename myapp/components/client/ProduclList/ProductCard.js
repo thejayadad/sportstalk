2c4e27bfaa0eeb4a7fc07820702751b518e39ce2
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-12-10 12:22:46 -0500

    update

--- myapp/components/client/ProduclList/ProductCard.js
+++ myapp/components/client/ProduclList/ProductCard.js
@@ -1,24 +1,20 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react';
+import ProductCard from './ProductCard';
+import Modal from '@/components/modals/Modal';
 
-const ProductCard = () => {
+
+const ProductList = () => {
+
   return (
-    <div>
-        <div className='relative'>
-            <div className='p-4 relative z-10'>
-                <img
-                className="w-full h-80   object-cover mb-4"
-                src="https://images.pexels.com/photos/940301/pexels-photo-940301.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                />
-            <div className='text-center'>
-            <h2 className="text-lg font-semibold mb-1">Title</h2>
-                    <p className="text-gray-500 mb-2">Description</p>
-                    <p className="text-blue-500 font-semibold"><span className='text-pink'>$</span>Price</p>
-            </div>
-            </div>
-        </div>
-    </div>
-  )
-}
+    <section className='max-w-screen-xl mx-auto px-4 py-8 cursor-pointer'>
+      <div className='grid grid-cols-2 items-center md:grid-cols-3 gap-4'>
+          <ProductCard  />
+      
+      </div>
 
-export default ProductCard
+   </section>
+  );
+};
+
+export default ProductList;
--- myapp/components/client/ProduclList/ProductList.js
+++ myapp/components/client/ProduclList/ProductList.js
@@ -1,34 +1,44 @@
 'use client'
-import React from 'react'
-import ProductCard from './ProductCard'
+import React, { useState } from 'react';
+import ProductCard from './ProductCard';
+import Modal from '@/components/modals/Modal';
 
 const ProductList = () => {
+  const [selectedProduct, setSelectedProduct] = useState(null);
+
+  const handleProductClick = (product) => {
+    setSelectedProduct(product);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedProduct(null);
+  };
+
   return (
-    <section className='max-w-screen-xl mx-auto px-4 py-8'>
-      <div className='text-center mb-20'>
-      <h1 className='sm:text-3xl text-2xl font-medium title-font text-primary mb-4'>
-        Fresh Menu Items
-      </h1>
-      <p  className='text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-secondary'>
-        See Our Current Menu Items
-      </p>
-      <div className="flex mt-6 justify-center">
-        <div className="w-16 h-1 rounded-full bg-pink inline-flex"></div>
+    <section className='max-w-screen-xl mx-auto px-4 py-8 cursor-pointer'>
+      {/* ... existing code ... */}
+      <div className='grid grid-cols-2 items-center md:grid-cols-3 gap-4'>
+        <ProductCard onClick={() => handleProductClick(product)} />
       </div>
-      </div>
-        <div className='grid grid-cols-2 items-center md:grid-cols-3 gap-4'>
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
 
-        </div>    
+      {/* Modal */}
+      {selectedProduct && (
+        <Modal isOpen={selectedProduct !== null} onClose={handleCloseModal}>
+          <img
+            src={selectedProduct.image}
+            alt={selectedProduct.title}
+            className='w-full h-64 object-cover mb-4'
+          />
+          <h2 className='text-xl font-semibold mb-2'>{selectedProduct.title}</h2>
+          <p className='text-gray-500 mb-4'>{selectedProduct.description}</p>
+          <p className='text-blue-500 font-semibold'>
+            <span className='text-pink'>$</span>
+            {selectedProduct.price}
+          </p>
+        </Modal>
+      )}
     </section>
-  )
-}
+  );
+};
 
-export default ProductList
+export default ProductList;
